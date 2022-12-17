c6759e5d34e1c0f489a8410a22d0a3b582602dcc
Pandy Knight <andylpk.247@gmail.com>
2022-12-16 22:05:30 -0500

    Added trailing "/" to status

--- app/routers/status.py
+++ app/routers/status.py
@@ -24,7 +24,9 @@ router = APIRouter()
 # --------------------------------------------------------------------------------
 
 @router.get("/status")
+@router.get("/status/", include_in_schema=False)
 @router.head("/status")
+@router.head("/status/", include_in_schema=False)
 def get_status():
   """
   Provides uptime information about the web service.
