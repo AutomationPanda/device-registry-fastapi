c735e17d8c62133deb1f84b3b4dbbd51e3d01cc5
Pandy Knight <andylpk.247@gmail.com>
2023-02-01 11:31:04 -0500

    Adjusted download test assertions

--- tests/integration/test_devices_report.py
+++ tests/integration/test_devices_report.py
@@ -15,9 +15,9 @@ def test_device_report_download(base_url, session, thermostat):
 
   # Verify response
   assert get_response.status_code == 200
-  assert 'text/plain' in get_response.headers['content-type']
-  assert int(get_response.headers['content-length']) > 0
+  assert get_response.headers['content-type'] == 'text/plain; charset=utf-8'
   assert get_response.headers['content-disposition'] == 'attachment; filename="Main Thermostat.txt"'
+  assert int(get_response.headers['content-length']) > 0
 
   # Verify content
   expected_report = \
